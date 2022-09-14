8a807fe965a6ea7ff408363f8d6627368639ef9a
Richard Lucas <richardluca@gmail.com>
2022-09-14 23:19:04 +0200

    command messages for reset and min value added

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 To do:
-- num > than display widht.
+- num > than display width.
 - message for negative numbers (min 0)
 - red when reset is or press - and already 0
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,57 @@
 "use strict";
 
 const counter = document.querySelector(".counter");
-const display = document.querySelector(".display");
+const displayContainer = document.querySelector(".display");
+const displayValue = document.querySelector(".display span");
+const buttons = document.querySelector(".buttons");
+
+let message = document.createElement("div");
 
 // starting value of counter
-let displayValue = 0;
+let Num = 999;
 
 counter.addEventListener("click", function (event) {
   updateCounter(event);
+  console.log("span", displayValue, displayValue.clientWidth);
+  console.log("display", displayContainer.clientWidth);
 });
 
 function updateCounter(event) {
+  message.remove();
+
   switch (event.target.closest("BUTTON").dataset.action) {
     case "+":
-      displayValue += 1;
-      display.style.color = "#89fc00";
+      if (displayContainer.clientWidth - displayValue.clientWidth > 21) {
+        buttons.insertAdjacentElement("afterbegin", message);
+        message.innerHTML = "Maximun value reached";
+        message.classList.add("message");
+        return;
+      }
+      Num += 10000;
+      displayValue.style.color = "#29bf12";
       break;
     case "-":
-      if (displayValue === 0) return;
-      displayValue -= 1;
-      display.style.color = "red";
+      if (Num === 0) {
+        buttons.insertAdjacentElement("afterbegin", message);
+        message.innerHTML = "Minimum value reached";
+        message.classList.add("message");
+        return;
+      }
+      Num -= 1;
+      displayValue.style.color = "red";
       break;
     case "reset":
-      if (displayValue === 0) return;
-      displayValue = 0;
-      display.style.color = "red";
+      if (Num === 0) {
+        buttons.insertAdjacentElement("afterbegin", message);
+        message.innerHTML = "Counter already reset";
+        message.classList.add("message");
+        return;
+      }
+      Num = 0;
+      displayValue.style.color = "red";
       break;
   }
-  display.innerHTML = displayValue;
-  setTimeout(() => (display.style.color = ""), "300");
+
+  displayValue.innerHTML = Num;
+  setTimeout(() => (displayValue.style.color = ""), "250");
 }
